351b723fd97333bff724ac35b58b5e8b3c9e6040
Aleksander Pres <75569465+Alexxx98@users.noreply.github.com>
2023-03-04 14:34:33 +0000

    docstrings added to app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+''' Import flask module '''
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    ''' printing greetings message to the page. '''
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True)    
+    app.run(debug=True)
