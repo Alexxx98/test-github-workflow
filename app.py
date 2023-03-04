cb5e6618f984d6d2030763c81e78d0658ba992a0
Aleksander Pres <75569465+Alexxx98@users.noreply.github.com>
2023-03-04 14:38:07 +0000

    docstrings modified

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+''' Flask app showing simple message. '''
+
 from flask import Flask
-''' Import flask module '''
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    ''' printing greetings message to the page. '''
+    ''' Printing greetings message to the page. '''
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == '__main__':
